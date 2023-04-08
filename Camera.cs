1ccbeabd2d5e408c00c8c29097d45b70d89e52b4
Ilya <rzhunovskiy@gmail.com>
2023-04-08 23:33:40 +0500

    some refactoring...

--- Camera.cs
+++ Camera.cs
@@ -27,5 +27,66 @@ namespace GLShooter
             this.direction = direction;
             plane = new Vector(0, 0.66);
         }
+
+        public int CastWall(int x, int width, int height, out int side)
+        {
+            var cameraX = 2 * x / (double)width - 1;
+            var rayDir = Direction + Plane * cameraX;
+            var deltaDist = new Vector(Math.Abs(1 / rayDir.X), Math.Abs(1 / rayDir.Y));
+            double perpWallDist;
+            var mapX = Position.X;
+            var mapY = Position.Y;
+            var stepX = rayDir.X < 0 ? -1 : 1;
+            var stepY = rayDir.Y < 0 ? -1 : 1;
+            var isHitted = false;
+            side = 0;
+            var sideDist = GetSideDist(rayDir, mapX, mapY, deltaDist);
+
+            while (!isHitted)
+            {
+                if (sideDist.X < sideDist.Y)
+                {
+                    sideDist.X += deltaDist.X;
+                    mapX += stepX;
+                    side = 0;
+                }
+                else
+                {
+                    sideDist.Y += deltaDist.Y;
+                    mapY += stepY;
+                    side = 1;
+                }
+
+                if (Map.mapObjects[(int)mapX][(int)mapY] > 0) isHitted = true;
+            }
+            if (side == 0)
+                perpWallDist = sideDist.X - deltaDist.X;
+            else
+                perpWallDist = sideDist.Y - deltaDist.Y;
+
+            return (int)(height / perpWallDist);
+
+        }
+
+        public void Rotate(double angle)
+        {
+            Plane.Rotate(angle);
+            Direction.Rotate(angle);
+        }
+
+        private Vector GetSideDist(Vector rayDir, double mapX, double mapY, Vector deltaDist)
+        {
+            var sideDist = new Vector(0, 0);
+            if (rayDir.X < 0)
+                sideDist.X = (Position.X - mapX) * deltaDist.X;
+            else
+                sideDist.X = (mapX + 1.0 - Position.X) * deltaDist.X;
+
+            if (rayDir.Y < 0)
+                sideDist.Y = (Position.Y - mapY) * deltaDist.Y;
+            else
+                sideDist.Y = (mapY + 1.0 - Position.Y) * deltaDist.Y;
+            return sideDist;
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -39,63 +39,7 @@ namespace GLShooter
         {
             for (var x = 0; x < this.Width; x++)
             {
-                var cameraX = 2 * x / (double)this.Width - 1;
-                var rayDir = camera.Direction + camera.Plane * cameraX;
-                var deltaDist = new Vector(Math.Abs(1 / rayDir.X), Math.Abs(1 / rayDir.Y));
-                var sideDist = new Vector(0, 0);
-                var perpWallDist = 0.0;
-
-                var mapX = camera.Position.X;
-                var mapY = camera.Position.Y;
-                var stepX = 0;
-                var stepY = 0;
-                var isHitted = false;
-                var side = 0;
-
-                if (rayDir.X < 0)
-                {
-                    stepX = -1;
-                    sideDist.X = (camera.Position.X - mapX) * deltaDist.X;
-                }
-                else
-                {
-                    stepX = 1;
-                    sideDist.X = (mapX + 1.0 - camera.Position.X) * deltaDist.X;
-                }
-                if (rayDir.Y < 0)
-                {
-                    stepY = -1;
-                    sideDist.Y = (camera.Position.Y - mapY) * deltaDist.Y;
-                }
-                else
-                {
-                    stepY = 1;
-                    sideDist.Y = (mapY + 1.0 - camera.Position.Y) * deltaDist.Y;
-                }
-
-                while (!isHitted)
-                {
-                    if (sideDist.X < sideDist.Y)
-                    {
-                        sideDist.X += deltaDist.X;
-                        mapX += stepX;
-                        side = 0;
-                    }
-                    else
-                    {
-                        sideDist.Y += deltaDist.Y;
-                        mapY += stepY;
-                        side = 1;
-                    }
-
-                    if (Map.mapObjects[(int)mapX][(int)mapY] > 0) isHitted = true;
-                }
-                if (side == 0)
-                    perpWallDist = sideDist.X - deltaDist.X;
-                else
-                    perpWallDist = sideDist.Y - deltaDist.Y;
-
-                var lineHeight = (int)(this.Height / perpWallDist);
+                var lineHeight = camera.CastWall(x, Width, Height, out int side);
                 var drawStart = -lineHeight / 2 + Height / 2;
 
                 if (drawStart < 0) drawStart = 0;
@@ -105,8 +49,7 @@ namespace GLShooter
                 e.Graphics.DrawLine(new Pen(color), x, drawStart, x, drawEnd);
 
             }
-            camera.Direction.Rotate(0.01);
-            camera.Plane.Rotate(0.01);
+            camera.Rotate(0.01);
         }
 
         private void TimerLoop(object sender, EventArgs e)
